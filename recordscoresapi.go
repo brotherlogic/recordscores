ad9752afe5e4e8034ac19155afb8f73a831837c9
Simon Tucker <brotherlogic@gmail.com>
2020-08-27 09:24:01 -0700

    First pass

---
+++ recordscoresapi.go
@@ -0,0 +1,10 @@
+package main
+
+import "golang.org/x/net/context"
+
+import rcpb "github.com/brotherlogic/recordcollection/proto"
+
+//ClientUpdate on an updated record
+func (s *Server) ClientUpdate(ctx context.Context, req *rcpb.ClientUpdateRequest) (*rcpb.ClientUpdateResponse, error) {
+	return &rcpb.ClientUpdateResponse{}, nil
+}
