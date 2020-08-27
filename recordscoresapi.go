9e266021c1275e4d4fe6ae8f17879cd242ba4eb8
Simon Tucker <brotherlogic@gmail.com>
2020-08-27 09:39:01 -0700

    Log when we update a score

--- recordscoresapi.go
+++ recordscoresapi.go
@@ -1,10 +1,15 @@
 package main
 
-import "golang.org/x/net/context"
+import (
+	"fmt"
 
-import rcpb "github.com/brotherlogic/recordcollection/proto"
+	"golang.org/x/net/context"
+
+	rcpb "github.com/brotherlogic/recordcollection/proto"
+)
 
 //ClientUpdate on an updated record
 func (s *Server) ClientUpdate(ctx context.Context, req *rcpb.ClientUpdateRequest) (*rcpb.ClientUpdateResponse, error) {
+	s.Log(fmt.Sprintf("Updating score for %v", req.GetInstanceId()))
 	return &rcpb.ClientUpdateResponse{}, nil
 }
