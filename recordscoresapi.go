65f402c822fef0557a40c98847c3d8f2c9b2b8b7
Simon Tucker <brotherlogic@gmail.com>
2020-09-08 16:14:48 -0700

    Adjustment

--- recordscoresapi.go
+++ recordscoresapi.go
@@ -11,7 +11,6 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	gdpb "github.com/brotherlogic/godiscogs"
 	rcpb "github.com/brotherlogic/recordcollection/proto"
 	rppb "github.com/brotherlogic/recordprocess/proto"
 	pb "github.com/brotherlogic/recordscores/proto"
@@ -116,20 +115,12 @@ func (s *Server) ClientUpdate(ctx context.Context, req *rcpb.ClientUpdateRequest
 		}
 	}
 
-	// Do we need to add the current score
-	conn, err := s.FDialServer(ctx, "recordcollection")
-	if err != nil {
-		return nil, err
-	}
-	defer conn.Close()
-	client := rcpb.NewRecordCollectionServiceClient(conn)
-
-	resp, err := client.GetRecord(ctx, &rcpb.GetRecordRequest{InstanceId: req.GetInstanceId()})
+	record, err := s.getRecord(ctx, req.GetInstanceId())
 	if err != nil {
 		return nil, err
 	}
 
-	if resp.GetRecord().GetRelease().GetRating() > 0 && !strings.HasPrefix(resp.GetRecord().GetMetadata().GetCategory().String(), "PRE") && resp.GetRecord().GetMetadata().GetCategory() != rcpb.ReleaseMetadata_UNLISTENED {
+	if record.GetRelease().GetRating() > 0 && !strings.HasPrefix(record.GetMetadata().GetCategory().String(), "PRE") && record.GetMetadata().GetCategory() != rcpb.ReleaseMetadata_UNLISTENED {
 		latest := ""
 		latestTime := int64(0)
 		for _, score := range subscores {
@@ -139,34 +130,24 @@ func (s *Server) ClientUpdate(ctx context.Context, req *rcpb.ClientUpdateRequest
 			}
 		}
 
-		if resp.GetRecord().GetMetadata().GetCategory().String() != latest {
+		if record.GetMetadata().GetCategory().String() != latest {
 			newScore := &pb.Score{
 				ScoreTime:  time.Now().Unix(),
-				Rating:     resp.GetRecord().GetRelease().GetRating(),
-				Category:   resp.GetRecord().GetMetadata().GetCategory(),
-				InstanceId: resp.GetRecord().GetRelease().GetInstanceId(),
+				Rating:     record.GetRelease().GetRating(),
+				Category:   record.GetMetadata().GetCategory(),
+				InstanceId: record.GetRelease().GetInstanceId(),
 			}
 			scores.Scores = append(scores.Scores, newScore)
 			s.Log(fmt.Sprintf("Adding score to db: %v -> %v", newScore, latest))
 
-			sc := float32(resp.GetRecord().GetRelease().GetRating())
+			sc := float32(record.GetRelease().GetRating())
 			count := float32(1)
 			for _, subscore := range subscores {
 				sc += float32(subscore.GetRating())
 				count++
 			}
 
-			client.UpdateRecord(ctx, &rcpb.UpdateRecordRequest{
-				Reason: "scores-push",
-				Update: &rcpb.Record{
-					Release: &gdpb.Release{
-						InstanceId: resp.GetRecord().GetRelease().GetInstanceId(),
-					},
-					Metadata: &rcpb.ReleaseMetadata{
-						OverallScore: sc / count,
-					},
-				},
-			})
+			s.updateOverallScore(ctx, record.GetRelease().GetInstanceId(), sc/count)
 
 			return &rcpb.ClientUpdateResponse{}, s.save(ctx, scores)
 		}
